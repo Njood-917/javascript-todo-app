201f6ba331887baece74e3530c1820c3fcd83a10
Njood-917 <njood9177@gmail.com>
2023-10-03 23:19:13 +0300

    last updtae

--- index.js
+++ index.js
@@ -1,61 +1,113 @@
- const addTask = document.getElementById('Add-task');
- const taskContainer = document.getElementById('task-container');
- const inputTask = document.getElementById('input-task');
+const addTask = document.getElementById('Add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+const checkboxes = document.querySelectorAll('.checkboxInput');
+const counter = document.getElementById('todo-counter');
+const completedCounter = document.getElementById('completed-counter');
 
- //event listener 
+//event listener 
 
- addTask.addEventListener ('click' , function(event){
+addTask.addEventListener ('click' , function(event){
 
-    let task = document.createElement('div');
-    task.classList.add('task');
-
-    let li = document.createElement ('li');
-    li.innerText = `${inputTask .value }`;
-
-    task.appendChild(li);
-
-    //add checkBtn 
-
-    let checkButton = document.createElement("button");
-    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-    checkButton.classList.add('checkTask');
-    task.appendChild(checkButton);
-
- //add deletBtn 
-    let deletButton = document.createElement("button");
-    deletButton.innerHTML = '<i class="fa-solid fa-x"></i>';
-    deletButton.classList.add('deletTask');
-    task.appendChild(deletButton);
+   event.preventDefault();
 
 
-    // convert
+   let task = document.createElement('div');
+   task.classList.add('task');
 
-    if (inputTask.value ===""){
-        alert ('please enter a valid value');
-    } else {
-        taskContainer.appendChild(task);
+   let li = document.createElement ('li');
+   li.innerText = `${inputTask .value }`;  // some change  li.innerText = inputTask.value;
+   task.appendChild(li);
 
-    }
-    inputTask.value ="";
+   // Append the task div to the task container
+   taskContainer.appendChild(task);
+
+   //add checkBtn 
+
+   let checkboxInput = document.createElement("input");
+   checkboxInput.type = "checkbox";
+   checkboxInput.classList.add('checkboxInput');
+   task.appendChild(checkboxInput);
 
 
-    //eventlistener for checkbutton
+//add deletButton
+   let deletButton = document.createElement("button");
+   deletButton.innerHTML = '<i class="fa-solid fa-x"></i>';
+   deletButton.classList.add('deletTask');
+   task.appendChild(deletButton);
 
-    checkButton.addEventListener ('click' , function(){
-        checkButton.parentElement.style.textDecoration ="line-through";
+   // Add editBtn 
+
+   let editButton = document.createElement("button");
+   editButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+   editButton.classList.add('editTask');
+   task.appendChild(editButton);
 
 
-    })
-     //eventlistener for deletbutton
-        
-    deletButton.addEventListener ('click' , function(e){
-        let target = e.target;
-        target.parentElement.parentElement.remove();
+   // convert
+
+   if (!inputTask.value){
+       alert ('please enter a valid value');
+       task.remove();  // Remove the task if input is empty
+   } else {
+      taskContainer.appendChild(task);
+
+    inputTask.value ="";  
+
+
+   //eventlistener for checkbutton
+
+   checkboxInput.addEventListener ('click' , function(){
+       if (li.style.textDecoration =="line-through") {
+           li.style.textDecoration = "none"
+       } else {
+           li.style.textDecoration = "line-through"
+       }
+       updateCounters(); 
+   }); 
    
+  
+    //eventlistener for deletbutton
+       
+   deletButton.addEventListener ('click' , function(e){
+       if (task){
+           task.remove();
+       }
+       updateCounters(); 
+   });
+   editButton.addEventListener ('click' , function(e){
+       
+       const input = document.createElement('input');
+       input.type = 'text';
+       input.value =  li.innerText;
+       li.replaceWith(input);
+       input.focus();
+       input.classList.add('inputTask');
 
-    });
+       input.addEventListener('blur', function () {
+           const updatedLi = document.createElement('li');
+           updatedLi.innerText = input.value;
+
+           input.replaceWith(updatedLi);
+
+       });
 
 
+});
+   
+   }
+   inputTask.value = "";
+   updateCounters();
+});
 
 
- });
+function updateCounters() {
+   const totalItems = document.querySelectorAll('.task');
+   const completedTasks = document.querySelectorAll('.task li[style="text-decoration: line-through;"]');
+  counter.textContent = totalItems.length;
+   completedCounter.textContent = completedTasks.length;
+  console.log(counter , completedTasks);
+
+}
+
+
